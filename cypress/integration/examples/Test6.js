f6b956143b742349020a3a3486ed36989d1fb1a1
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-13 22:05:48 +0530

    mousehover

---
+++ cypress/integration/examples/Test6.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+    // cy.get('div.mouse-hover-content').invoke('show')
+    cy.contains('Top').click({ force: true })
+    cy.url().should('include', 'top')
+  })
+})

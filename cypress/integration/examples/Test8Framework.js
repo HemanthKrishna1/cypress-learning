fc21a1d974746f5e5dc9f5d9ac734668582d0068
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-14 12:49:45 +0530

    added custom command in commands.js

--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -14,8 +14,16 @@ describe('My First Test', () => {
     cy.get(":nth-child(1) > .form-control").type(this.data.name)
     cy.get('select').select(this.data.gender)
 
-    // cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
 
-    // cy.get()
+    cy.get(":nth-child(1) > .form-control").should('have.attr', 'minlength', '2')
+    cy.get('#inlineRadio3').should('be.disabled')
+
+
+    cy.get(':nth-child(2) > .nav-link').click()
+
+    cy.selectProduct('Blackberry')
+    cy.selectProduct('Nokia Edge')
+
   })
 })

607889a84a65af152f34efcb98c024efbc239c39
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-14 18:34:04 +0530

    added deafultTimeOut and pageLoadTimeOut in cypress.config.js

--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -18,23 +18,32 @@ describe('My First Test', () => {
 
     homePage.getEditBox().type(this.data.name)
     homePage.getGender().select(this.data.gender)
-
     homePage.getTwoDateBinding().should('have.value', this.data.name)
-
     homePage.getEditBox().should('have.attr', 'minlength', '2')
-
     homePage.getEntrepreneaur().should('be.disabled')
+    Cypress.config('defaultCommandTimeout', 8000)
 
     homePage.getShopTab().click()
 
     this.data.productName.forEach(function (element) {
       cy.selectProduct(element)
     })
+
     productPage.checkOutButton().click()
+    cy.contains('Checkout').click()
+    cy.get('#country').type('India')
+    cy.get('.suggestions > ul > li > a').click()
+    cy.get('#checkbox2').click({ force: true })
+    cy.get('input[type="submit"]').click()
+    // cy.get('.alert').should('have.text', '\n          ×\n          Success! Thank you! Your order will be delivered in next few weeks :-).\n        ')
+
+    cy.get('.alert').then(function (el) {
+      const txt = el.text();
+      expect(txt.includes('Success')).to.be.true
+    })
     // cy.selectProduct('Blackberry')
 
     // cy.selectProduct('Nokia Edge')
-
 
   })
 })

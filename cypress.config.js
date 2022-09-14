c636a6e756351b3157241ec0b869c29a78c5b84f
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-14 22:19:07 +0530

    added environmental variables

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
   pageLoadTimeout: 30000,
+  env: {
+    url: 'https://rahulshettyacademy.com',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
